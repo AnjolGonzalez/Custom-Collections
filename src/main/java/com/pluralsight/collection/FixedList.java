4128e1a6bbe18bf6dc7f9655d1e0504130e30795
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-11-15 13:20:42 -0500

    Completed

--- src/main/java/com/pluralsight/collection/FixedList.java
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,6 +1,24 @@
-package com.pluralsightCollection;
+package com.pluralsight.collection;
+import java.util.ArrayList;
 import java.util.List;
-public class FixedList {
+public class FixedList<T> {
     List<T> items;
     int maxSize;
+
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(T item) {
+        if (items.size() < maxSize) {
+            items.add(item);
+        } else {
+            System.out.println("Cannot add more than the maxsize.");
+        }
+    }
+
+    public List<T> getItems() {
+        return this.items;
+    }
 }
--- src/main/java/com/pluralsight/collection/Main.java
+++ src/main/java/com/pluralsight/collection/Main.java
@@ -1,5 +1,23 @@
 package com.pluralsight.collection;
 
-public class T extends FixedList{
+import java.time.LocalDate;
 
+public class Main{
+    public static void main(String[] args) {
+        FixedList<Integer> numbers = new FixedList<>(5);
+        numbers.add(10);
+        numbers.add(3);
+        numbers.add(92);
+        numbers.add(43);
+        numbers.add(0);
+
+        System.out.println(numbers.getItems().size());
+
+        FixedList<LocalDate> dates = new FixedList<>(4);
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+
+    }
 }
