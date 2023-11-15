78e82afd8e6d4c7acece2ac342075a90ed5f981d
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-11-15 13:20:03 -0500

    Completed

---
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -0,0 +1,6 @@
+package com.pluralsightCollection;
+import java.util.List;
+public class FixedList {
+    List<T> items;
+    int maxSize;
+}
---
+++ src/main/java/com/pluralsight/collection/Main.java
@@ -0,0 +1,5 @@
+package com.pluralsight.collection;
+
+public class T extends FixedList{
+
+}
